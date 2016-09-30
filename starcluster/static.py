a70e29b7e83dc42b23cda9171f05531d74e405c6
Alex Rothberg <arothberg@4catalyzer.com>
2016-09-30 22:20:34 +0000

    new node type

--- starcluster/static.py
+++ starcluster/static.py
@@ -143,6 +143,7 @@ INSTANCE_TYPES = {
     'cg1.4xlarge': ['x86_64'],
     'g2.2xlarge': ['x86_64'],
     'g2.8xlarge': ['x86_64'],
+    'p2.xlarge': ['x86_64'],
     'cr1.8xlarge': ['x86_64'],
     'hi1.4xlarge': ['x86_64'],
     'hs1.8xlarge': ['x86_64'],
@@ -174,7 +175,7 @@ SEC_GEN_TYPES = ['m3.medium', 'm3.large', 'm3.xlarge', 'm3.2xlarge']
 
 CLUSTER_COMPUTE_TYPES = ['cc1.4xlarge', 'cc2.8xlarge']
 
-CLUSTER_GPU_TYPES = ['g2.2xlarge', 'g2.8xlarge', 'cg1.4xlarge']
+CLUSTER_GPU_TYPES = ['p2.xlarge', 'g2.2xlarge', 'g2.8xlarge', 'cg1.4xlarge']
 
 CLUSTER_HIMEM_TYPES = ['cr1.8xlarge']
 

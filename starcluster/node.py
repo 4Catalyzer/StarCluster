3233f52a0b63d03fe72908d4da519cbadd9cf617
Alex Rothberg <agrothberg@gmail.com>
2017-05-08 12:31:39 -0400

    remove commented

--- starcluster/node.py
+++ starcluster/node.py
@@ -718,8 +718,6 @@ class Node(object):
         with self.ssh.remote_file(DUMMY_EXPORT_FILE, 'w') as dummyf:
             dummyf.write(DUMMY_EXPORT_LINE)
         self.ssh.execute('service nfs-kernel-server start')
-        # self.ssh.execute('/etc/init.d/nfs-kernel-server start')
-        # self.ssh.execute('/etc/init.d/nfs start')
         self.ssh.execute('rm -f %s' % DUMMY_EXPORT_FILE)
         self.ssh.execute('rm -rf %s' % DUMMY_EXPORT_DIR)
         self.ssh.execute('exportfs -fra')
@@ -733,7 +731,6 @@ class Node(object):
         """
 
         self.ssh.execute('service rpcbind start', ignore_exit_status=True)
-        # self.ssh.execute('/etc/init.d/portmap start')
         # TODO: move this fix for xterm somewhere else
         self.ssh.execute('mount -t devpts none /dev/pts',
                          ignore_exit_status=True)
